ef802d57b6b8d45fee555f4119ac5da41ba4e48d
vanh7979 <137498913+vanh7979@users.noreply.github.com>
2024-03-14 08:33:30 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,25 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
 
-        Bài làm viết tại đây
+        try (BufferedReader readd = new BufferedReader(new FileReader("input.txt"));
+             BufferedWriter writee = new BufferedWriter(new FileWriter("output.txt"))) {
 
+            String line;
+            while ((line = readd.readLine()) != null) {
+                line = line.replaceAll("Nha Trang", "Vũng Tàu");
+                writee.write(line);
+                writee.newLine();
+            }
+
+            System.out.println("Complete!");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Không tìm thấy file đầu vào hoặc đầu ra.");
+        } catch (IOException e) {
+            System.out.println("Có lỗi xảy ra trong quá trình đọc ghi file.");
+        }
+    
     }
 }
